9bf5171479da945863c58b804e0ceab099003f12
SheikhAnasDanial <2022887714@student.uitm.edu.my>
2025-03-26 13:37:29 +0800

    update

--- test.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyCCMqub_m4O8umGE_Rw_iuGIDkxPBl7QKE")
-
-models = genai.list_models()
-for model in models:
-    print(model.name)
